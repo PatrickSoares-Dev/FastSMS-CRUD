558e838971c0e3f000d6207fb4f3a9055a2ef0f3
Patrick Oliveira <patrick.oliveira@telecall.com>
2023-09-04 17:17:16 -0300

    v1 Funcionando

--- assets/js/scripts_pages/register.js
+++ assets/js/scripts_pages/register.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const inputGroup = field.closest('.input-group'); // Encontra o elemento .input-group pai
 
         if (value === '') {
-            inputGroup.classList.add('is-invalid');
+            field.classList.add('is-invalid');
 
             // Adiciona a mensagem de erro à div
             const errorMessage = document.createElement('div');
